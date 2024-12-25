cef37d5381c676119d4190bc1fcfa356424f2823
hokkani <niko.hokkanen@logisnext.eu>
2024-12-26 00:06:57 +0200

    Added XSD schema-based validation

---
+++ XmlPrettify/XmlPrettify.Client/Models/MonacoValidationError.cs
@@ -0,0 +1,11 @@
+﻿namespace XmlPrettify.Components.Models
+{
+    public class MonacoValidationError
+    {
+        public string Message { get; set; } = string.Empty;
+        public int StartLineNumber { get; set; }
+        public int StartColumn { get; set; }
+        public int EndLineNumber { get; set; }
+        public int EndColumn { get; set; }
+    }
+}
